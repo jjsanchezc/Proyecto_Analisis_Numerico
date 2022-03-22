21c343742b502c119c80d45596eeca8994ec53f3
jjsanchezc <68928367+jjsanchezc@users.noreply.github.com>
2022-03-22 01:46:55 -0500

    Update settings.py

--- Proyecto_Analisis_Numerico/settings.py
+++ Proyecto_Analisis_Numerico/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@9j!s8+vioimq41+t4=@(t)9fzqrv((xk%=)(m&^7)ig#jsfyz'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
