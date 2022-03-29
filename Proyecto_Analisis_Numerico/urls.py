e541ec660cb6c01cb93ba65379f4e29e22e1690f
jjsanchezc <jjsanchezc@eafit.edu.co>
2022-03-29 04:27:48 -0500

    commit on "master"

--- Proyecto_Analisis_Numerico/urls.py
+++ Proyecto_Analisis_Numerico/urls.py
@@ -20,4 +20,5 @@ from metodos import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home),
+    path('busquedas-incrementales/',views.busIncr),
 ]
--- metodos/views.py
+++ metodos/views.py
@@ -4,8 +4,9 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    print("si está cargando")
     return render(request,'home.html')
 
-def segundaPantalla(request):
-    return HttpResponse("holi")
+def busIncr(request):
+    if request.method=="POST":
+        print("entraaaaaaa")
+    return render(request,'metodos/busquedasIncr.html')
