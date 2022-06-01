abc743ac0e3650372b3e1fb2f19408099bed4975
jjsanchezc <jjsanchezc@eafit.edu.co>
2022-06-01 03:57:34 -0500

    jacobi

--- metodos/metodosNumericos/cap2/metodos.py
+++ metodos/metodosNumericos/cap2/metodos.py
@@ -1,4 +1,5 @@
 import base64
+from turtle import end_poly
 from numpy.linalg import inv, eigvals, norm
 import numpy as np
 from numpy import array, zeros, fabs, linalg
@@ -51,6 +52,7 @@ def jacobi(a,b,x0,tol,n):
     d = a + l + u
     t = np.matmul(inv(d), l + u)
     c = np.matmul(inv(d), b)
+    msg=None
     if max(abs(eigvals(t))) > 1:
         msg='la funcion no converge'
         return msg
@@ -64,6 +66,7 @@ def jacobi(a,b,x0,tol,n):
     xn = np.matmul(t, x0) + c
     cont = 0
     e = 1000
+    
     while (x0 != xn).all() and cont < n and e > tol:
         x0 = xn
         xn = np.matmul(t, x0) + c
@@ -74,9 +77,12 @@ def jacobi(a,b,x0,tol,n):
         tempMapIterData['xn'] = str(xn)
         tempMapIterData['E'] = str(e)
         tableListData.append(tempMapIterData.copy())
+        msg=None
         for i in tableListData:
             print(i)
+            msg=i
         tempMapIterData.clear()
+    return msg 
 
 def seidel():
     pass
